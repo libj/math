a0029db201e0f293bfe856e694605f7d2c2f8248
Seva Safris <seva@safris.org>
2019-04-28 01:03:45 +0200

    Repackage to ext

--- src/main/java/org/openjax/ext/math/SafeMath.java
+++ src/main/java/org/openjax/ext/math/SafeMath.java
@@ -14,7 +14,7 @@
  * program. If not, see <http://opensource.org/licenses/MIT/>.
  */
 
-package org.openjax.standard.math;
+package org.openjax.ext.math;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
