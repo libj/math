385a5ec34dee8e7e5fa8a64bbeb1ece28aff9b6c
Seva Safris <seva@safris.org>
2020-09-01 15:33:39 +0700

    Improve E10 methods

--- src/main/java/org/libj/math/FastMath.java
+++ src/main/java/org/libj/math/FastMath.java
@@ -16,6 +16,8 @@
 
 package org.libj.math;
 
+import java.util.Arrays;
+
 /**
  * Alternative functions supplementing those in {@link java.lang.Math} that
  * offer higher performance algorithms.
@@ -25,7 +27,116 @@ public final class FastMath {
    * The values of <code>10<sup>n</sup></code> for {@code n} between {@code 0}
    * and {@code 18}.
    */
-  public static final long[] e10 = {1L, 10L, 100L, 1000L, 10000L, 100000L, 1000000L, 10000000L, 100000000L, 1000000000L, 10000000000L, 100000000000L, 1000000000000L, 10000000000000L, 100000000000000L, 1000000000000000L, 10000000000000000L, 100000000000000000L, 1000000000000000000L};
+  public static final long[] E10 = {1L, 10L, 100L, 1000L, 10000L, 100000L, 1000000L, 10000000L, 100000000L, 1000000000L, 10000000000L, 100000000000L, 1000000000000L, 10000000000000L, 100000000000000L, 1000000000000000L, 10000000000000000L, 100000000000000000L, 1000000000000000000L};
+  private static volatile double[] doubleE10 = {1};
+
+  /** This value was tuned with {@link FastMathTest#testDoubleE10} */
+  @SuppressWarnings("javadoc")
+  private static final int MAX_E10_DOUBLE = 1 << 15;
+
+  private static volatile int[][] bigIntE10 = {new int[] {1, 1}};
+
+  static {
+    doubleE10(1024);
+    E10(16);
+  }
+
+  /**
+   * Return <code>10<sup>n</sup></code>, as a {@code double}, expanding the
+   * underlying {@link #doubleE10} array if necessary.
+   *
+   * @param n The power of ten to be returned (>= 0).
+   * @return A {@linkplain BigInt#val() value-encoded number} with the value
+   *         (10<sup>n</sup>).
+   * @throws ArrayIndexOutOfBoundsException If {@code n} is negative.
+   */
+  public static double doubleE10(final int n) {
+    if (n >= MAX_E10_DOUBLE)
+      return StrictMath.pow(10, n);
+
+    double[] pows = doubleE10;
+    if (n < pows.length)
+      return pows[n];
+
+    synchronized (doubleE10) {
+      int curLen = pows.length;
+      // The following comparison and the above synchronized statement is
+      // to prevent multiple threads from expanding the same array.
+      if (curLen <= n) {
+        int newLen;
+        if (n < 256) {
+          newLen = curLen * 2;
+          while (newLen <= n)
+            newLen *= 2;
+        }
+        else {
+          newLen = n + 64;
+        }
+
+        pows = Arrays.copyOf(pows, newLen);
+        for (; curLen < newLen; ++curLen)
+          pows[curLen] = StrictMath.pow(10, curLen);
+
+        // Based on the following facts:
+        // 1. pows is a private local variable;
+        // 2. the following store is a volatile store.
+        // Thus the newly created array elements can be safely published.
+        doubleE10 = pows;
+      }
+
+      return pows[n];
+    }
+  }
+
+  /**
+   * Return <code>10<sup>n</sup></code>, as a {@linkplain BigInt#val()
+   * value-encoded number}, expanding the underlying {@link #bigIntE10} array if
+   * necessary.
+   *
+   * @param n The power of ten to be returned (>= 0).
+   * @return A {@linkplain BigInt#val() value-encoded number} with the value
+   *         (10<sup>n</sup>).
+   * @throws ArrayIndexOutOfBoundsException If {@code n} is negative.
+   */
+  public static int[] E10(final int n) {
+    int[][] pows = bigIntE10;
+    if (n < pows.length)
+      return pows[n];
+
+    synchronized (bigIntE10) {
+      int curLen = pows.length;
+      // The following comparison and the above synchronized statement is
+      // to prevent multiple threads from expanding the same array.
+      if (curLen <= n) {
+        int newLen;
+        if (n < 256) {
+          newLen = curLen * 2;
+          while (newLen <= n)
+            newLen *= 2;
+        }
+        else {
+          newLen = n + 64;
+        }
+
+        int[] val;
+        pows = Arrays.copyOf(pows, newLen);
+        for (int len; curLen < newLen; ++curLen) {
+          val = pows[curLen - 1];
+          len = val[0];
+          val = len + 2 >= val.length ? BigInt.reallocExact(val, len + 1, len + 2) : val.clone();
+          val[0] = BigIntMultiplication.umul0(pows[curLen] = val, 1, len, 10);
+        }
+
+        // Based on the following facts:
+        // 1. pows is a private local variable;
+        // 2. the following store is a volatile store.
+        // Thus the newly created array elements can be safely published.
+        bigIntE10 = pows;
+      }
+
+      return pows[n];
+    }
+  }
 
   /**
    * Returns the base 2 logarithm of positive {@code int} values, and {@code 0}
