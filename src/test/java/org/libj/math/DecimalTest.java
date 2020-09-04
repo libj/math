5cf2865cf0ecb5ab594133b59fb3cef51e2d3c52
Seva Safris <seva@safris.org>
2020-09-04 20:38:10 +0700

    Restrict MAX_SCALE_BITS for CI test

--- src/test/java/org/libj/math/DecimalTest.java
+++ src/test/java/org/libj/math/DecimalTest.java
@@ -22,7 +22,7 @@ import org.libj.console.Ansi;
 import org.libj.console.Ansi.Color;
 
 abstract class DecimalTest extends NumericCaseTest {
-  final int MAX_SCALE_BITS = isCI ? 13 : Decimal.MAX_SCALE_BITS;
+  final int MAX_SCALE_BITS = isCI ? 10 : Decimal.MAX_SCALE_BITS;
 
   static int skip(final byte scaleBits) {
     return (int)Math.pow(scaleBits, scaleBits / 9d);
