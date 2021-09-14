0c4ba6bfacd09dda84bcbb59ff910493e771a248
Seva Safris <seva@safris.org>
2021-09-15 00:05:51 +0700

    Refactor Number.Compound -> Number.Composite

--- src/test/java/org/libj/math/BigIntStudy.java
+++ src/test/java/org/libj/math/BigIntStudy.java
@@ -33,7 +33,7 @@ public class BigIntStudy extends BigIntTest {
    * Length and sig separated, thus sig can be -1, 0, or 1. A length of 0
    * means sig must be 0, which means code has to check for this
    * alignment.</li>
-   * <li><ins>Compound: {@code int[] {sigLen, ..}}</ins><br>
+   * <li><ins>Composite: {@code int[] {sigLen, ..}}</ins><br>
    * Length and sig are combined, whereby a positive length means a sig =
    * 1, and a negative length a sig = -1. A length of 0 means the value is
    * zero, and sig ends up as 1. No alignment checks are necessary.</li>
@@ -42,14 +42,14 @@ public class BigIntStudy extends BigIntTest {
   @Test
   public void testLengthSignum() {
     final int[] v = new int[10];
-    test("length signum: regular vs compound").withCases(
+    test("length signum: regular vs composite").withCases(
       i("Regular", a -> { v[0] = Math.abs(a / 2); v[1] = Integer.compare(a, 0); return v; }, (int[] a) -> {
         int len = a[0];
         int sig = a[1];
 
         return len * sig;
       }, Integer::valueOf),
-      i("Compound", a -> { v[0] = a / 2; return v; }, (int[] a) -> {
+      i("Composite", a -> { v[0] = a / 2; return v; }, (int[] a) -> {
         int sig, len = a[0];
         if (len < 0) {
           len = -len;
